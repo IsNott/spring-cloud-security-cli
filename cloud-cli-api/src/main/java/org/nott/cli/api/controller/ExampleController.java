542e9739606a13c739a866a468cd84821e208ddf
Nott <nott@163.com>
2024-04-18 15:47:00 +0800

    feat: CommonMapper execute user's sql.Jackson object mapper trans Long type to String type.
    (cherry picked from commit a88b1c2d43683810fb8c316f408d12c01327b410)

--- cloud-cli-api/src/main/java/org/nott/cli/api/controller/ExampleController.java
+++ cloud-cli-api/src/main/java/org/nott/cli/api/controller/ExampleController.java
@@ -1,17 +1,26 @@
 package org.nott.cli.api.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import jakarta.annotation.Resource;
 import org.nott.cli.common.model.Result;
+import org.nott.cli.service.mapper.CommonMapper;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/your-api/")
 public class ExampleController {
 
+    @Resource
+    private CommonMapper commonMapper;
+
     @RequestMapping("test")
-    public Result test(@RequestBody JSONObject jsonObject){
-        return Result.success();
+    public Result<List<Map<String,Object>>> test(@RequestBody JSONObject jsonObject){
+        List<Map<String, Object>> result = commonMapper.executeSql("select * from test");
+        return Result.success(result);
     }
 }
