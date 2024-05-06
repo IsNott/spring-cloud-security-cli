0108b29d3415a26846de257439fc6a843e75b8ba
Nott <nott@163.com>
2024-05-06 14:48:03 +0800

    delete: nacos example config file.

--- cloud-cli-api/src/main/java/org/nott/Application.java
+++ cloud-cli-api/src/main/java/org/nott/Application.java
@@ -3,7 +3,7 @@ package org.nott;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication(scanBasePackages = {"org.nott.cli"})
 @MapperScan(basePackages = {"org.nott.cli.service.mapper.**"})
