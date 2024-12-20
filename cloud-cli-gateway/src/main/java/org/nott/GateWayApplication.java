c5108827286f0186471ae0e3f4f90b96952544a2
Nott <nott@163.com>
2024-12-20 10:45:45 +0800

    feat: Spring-cloud-gateway And Spring actuator.

--- cloud-cli-gateway/src/main/java/org/nott/GateWayApplication.java
+++ cloud-cli-gateway/src/main/java/org/nott/GateWayApplication.java
@@ -4,10 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.GatewayDiscoveryClientAutoConfiguration;
 
 @Slf4j
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages = "org.nott.gateway")
+@SpringBootApplication(scanBasePackages = "org.nott.gateway", exclude = GatewayDiscoveryClientAutoConfiguration.class)
 public class GateWayApplication {
         public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
